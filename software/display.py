d3fe05e9d77849e60988c6f749e6c3faa79d82ce
Tom <tom@tdpain.net>
2020-06-26 20:20:14 +0100

    Loading sprite test

--- software/display.py
+++ software/display.py
@@ -1,10 +1,15 @@
-from PIL import Image
-from PIL import ImageDraw
 import adafruit_ssd1306
 import board
+from PIL import Image
+from PIL import ImageDraw
+
+
+"""
+Classes and functions related to display drawing/generation
+"""
 
 class Display:
-    def __init__(self, i2c_addr, disp_width=128, disp_height=64):
+    def __init__(self, i2c_addr, disp_width, disp_height):
         self.width = disp_width
         self.height = disp_height
 
@@ -13,16 +18,23 @@ class Display:
         self.disp.show()
 
     def clear(self):
-        d = Drawing()
+        d = Drawing(self)
         d.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
-        self.show_image(d)
+        self.show_drawing(d)
 
-    def show_image(self, drawing):
-        self.disp.image(drawing.image)
+    def show_image(self, im):
+        self.disp.image(im)
         self.disp.show()
+
+    def show_drawing(self, drawing):
+        self.show_image(drawing.image)
 
 
 class Drawing:
-    def __init__(self, width=128, height=64):
-        self.image = Image.new("1", (width, height))
+    def __init__(self, display):
+        self.display = display
+        self.image = Image.new("1", (display.width, display.height))
         self.draw = ImageDraw.Draw(self.image)
+
+    def show(self):
+        self.display.show_image(self)
--- software/main.py
+++ software/main.py
@@ -1,37 +1,55 @@
+import time
+
+from PIL import Image
+
+from constants import *
 from display import Display, Drawing
 
-import time
-import math
-import gpiozero
-from PIL import ImageFont
 
-wheel_circ = (math.pi * 26) / 63360
-last_active = None
-b = gpiozero.Button(22)
-def calculate_speed(time):
-    return wheel_circ / (time / 60 / 60)
+class MasterClass:
+    disp = Display(DISPLAY_I2C_ADDR, DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    currentSpeed = 0
 
-disp = Display(0x3c)
 
-width = disp.width
-height = disp.height
+m = MasterClass()
 
-image = Drawing()
+disp = Display(0x3c, DISPLAY_WIDTH, DISPLAY_HEIGHT)
 
-image.draw.rectangle((0,0,width,height), outline=0, fill=0)
+# font = ImageFont.truetype(ASSET_UBUNTU_MONO, 15)
+#
+# image = Drawing(disp)
+# image.draw.rectangle((0,0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=255)
+# image.draw.text((2, 2), "Hello world!",  font=font, fill=0)
+# image.show()
+#
+# time.sleep(2)
 
-font = ImageFont.truetype("umono.ttf", 15)
 
-image.draw.text((2, 2), "Hello world!",  font=font, fill=255)
+def grow_to_display_footprint(im:Image.Image):
+    insert_w, insert_h = im.size
 
-disp.show_image(image)
+    position_w = int((DISPLAY_WIDTH - insert_w) / 2)
+    position_h = int((DISPLAY_HEIGHT - insert_h) / 2)
 
-time.sleep(2)
+    new_im = Image.new("1", (DISPLAY_WIDTH, DISPLAY_WIDTH))
+    new_im.paste(im, (position_w, position_h))
 
-disp.clear()
+    return new_im
 
+
+im1 = grow_to_display_footprint(Image.open("../assets/loading_1.png").convert("1"))
+im2 = grow_to_display_footprint(Image.open("../assets/loading_2.png").convert("1"))
+
+while True:
+    disp.show_image(im1)
+    time.sleep(0.5)
+    disp.clear()
+    disp.show_image(im2)
+
+
+# This will be useful later on
 # while True:
-#     draw.rectangle((0,0,width,height), outline=0, fill=0)
+#     draw.rectangle((0,0,DISPLAY_WIDTH,DISPLAY_HEIGHT), outline=0, fill=0)
 #
 #     if b.wait_for_press(timeout=5):
 #         if last_active is None:
