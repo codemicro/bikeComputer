196ad2ad658474eae0cadcabbf5179eb581a1487
Tom <tom@tdpain.net>
2020-06-27 14:05:09 +0100

    Add constants for blank images

--- software/constants.py
+++ software/constants.py
@@ -1,6 +1,8 @@
 import json
 import math
 
+from PIL import Image, ImageDraw, ImageChops
+
 with open("../settings.json") as f:
     loaded_settings = json.load(f)
 
@@ -17,3 +19,10 @@ DISPLAY_WIDTH = loaded_settings["display"]["width"]
 # Constants for calculating speed
 SPEED_WHEEL_DIAMETER = loaded_settings["wheelDiameter"]  # Inches
 SPEED_WHEEL_CIRCUMFERENCE = (math.pi * SPEED_WHEEL_DIAMETER) / 63360  # In miles so result is miles per hour
+
+# Constants for UI prefabs
+UI_BLANK_IMAGE = Image.new("1", (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+ImageDraw.Draw(UI_BLANK_IMAGE).rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=255)
+
+UI_INVERTED_BLANK_IMAGE = Image.new("1", (DISPLAY_WIDTH, DISPLAY_HEIGHT))
+ImageDraw.Draw(UI_BLANK_IMAGE).rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=0)
