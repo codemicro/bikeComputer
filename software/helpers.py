aedeb30570a5c6c18f556aab1e0960c284059ead
Tom <tom@tdpain.net>
2020-06-27 14:07:05 +0100

    Move functions to helpers.py file

--- software/helpers.py
+++ software/helpers.py
@@ -1,10 +1,102 @@
-import constants
-
+from constants import *
+from PIL import Image, ImageFont, ImageChops
+from display import Drawing
 
 def calculate_speed(time):
     """
     Calculate speed based on time in seconds for one wheel rotation
+
     :param time: float, number of seconds between wheel rotation
     :return: float, speed in MPH
     """
-    return constants.SPEED_WHEEL_CIRCUMFERENCE / (time / 60 / 60)
+    return SPEED_WHEEL_CIRCUMFERENCE / (time / 60 / 60)
+
+
+def grow_to_display_footprint(im: Image.Image, background=0):
+    """
+    The grows the image supplied to the resolution of the display
+
+    :param im: image to grow
+    :param background: colour to fill the background of the image with
+    :return: new image
+    """
+
+    insert_w, insert_h = im.size
+
+    position_w = int((DISPLAY_WIDTH - insert_w) / 2)
+    position_h = int((DISPLAY_HEIGHT - insert_h) / 2)
+
+    d = Drawing()
+    d.draw.rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=background)
+    d.image.paste(im, (position_w, position_h))
+
+    return d.image
+
+
+def crop_whitespace(im):
+    """
+    Crops whitespace around a supplied image
+
+    :param im: image to crop
+    :return: cropped image
+    """
+
+    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
+    diff = ImageChops.difference(im, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return im.crop(bbox)
+    else:
+        return im
+
+
+def add_text_to_image(im: Image.Image, text: str, font: ImageFont.ImageFont, text_fill: int = 255,
+                      text_on_top: bool = True,
+                      gap: int = 4):
+    """
+    Add text to an image
+
+    :param im: image to add text too
+    :param text: text to add to image
+    :param font: font to use
+    :param text_fill: colour of the text (0 to 255)
+    :param text_on_top: if the text should be placed above the image
+    :param gap: gap between image and text in pixels
+    :return: image with text
+    """
+
+    text_img = Drawing()
+
+    text_img.draw.text((0, 0), text, font=font, fill=text_fill)
+    text_img.image = crop_whitespace(text_img.image)
+
+    new_height = text_img.image.size[1] + gap + im.size[1]
+
+    if text_img.image.size[0] > im.size[0]:
+        new_width = text_img.image.size[0]
+    else:
+        new_width = im.size[0]
+
+    final_drawing = Drawing(Image.new("1", (new_width, new_height)))
+
+    # Determine image placement
+
+    image_placement_x = int((new_width - im.size[0]) / 2)
+    image_placement_y = 0
+
+    if text_on_top:
+        image_placement_y += gap + text_img.image.size[1]
+
+    # Determine text placement
+
+    text_placement_x = int((new_width - text_img.image.size[0]) / 2)
+    text_placement_y = 0
+
+    if not text_on_top:
+        text_placement_y = im.size[1] + gap
+
+    final_drawing.image.paste(im, (image_placement_x, image_placement_y))
+    final_drawing.image.paste(text_img.image, (text_placement_x, text_placement_y))
+
+    return final_drawing.image
--- software/main.py
+++ software/main.py
@@ -1,7 +1,10 @@
+import sys
 import time
 
+import signal
 from PIL import Image, ImageFont, ImageChops
 
+import helpers
 from constants import *
 from display import Display, Drawing
 
@@ -15,6 +18,15 @@ m = MasterClass()
 
 disp = Display(0x3c, DISPLAY_WIDTH, DISPLAY_HEIGHT)
 
+
+def sigint_handler(_=None, __=None):
+    print("Attempting gracefully shutting down...")
+    disp.clear()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, sigint_handler)
+
 # font = ImageFont.truetype(ASSET_UBUNTU_MONO, 15)
 #
 # image = Drawing(disp)
@@ -25,82 +37,24 @@ disp = Display(0x3c, DISPLAY_WIDTH, DISPLAY_HEIGHT)
 # time.sleep(2)
 
 
-def grow_to_display_footprint(im:Image.Image, background=0):
-    insert_w, insert_h = im.size
-
-    position_w = int((DISPLAY_WIDTH - insert_w) / 2)
-    position_h = int((DISPLAY_HEIGHT - insert_h) / 2)
-
-    d = Drawing(disp)
-    d.draw.rectangle((0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT), outline=0, fill=background)
-    d.image.paste(im, (position_w, position_h))
-
-    return d.image
-
-
-def crop_whitespace(im):
-    bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
-
-
-def add_text_to_image(im, text, font, text_fill=255, text_on_top=True, gap=4):
-    text_img = Drawing(disp)
-
-    text_img.draw.text((0, 0), text, font=font, fill=text_fill)
-    text_img.image = crop_whitespace(text_img.image)
-
-    new_height = text_img.image.size[1] + gap + im.size[1]
-
-    if text_img.image.size[0] > im.size[0]:
-        new_width = text_img.image.size[0]
-    else:
-        new_width = im.size[0]
-
-    final_drawing = Drawing(disp, Image.new("1", (new_width, new_height)))
-
-    # Determine image placement
-
-    image_placement_x = int((new_width - im.size[0]) / 2)
-    image_placement_y = 0
-
-    if text_on_top:
-        image_placement_y += gap + text_img.image.size[1]
-
-    # Determine text placement
-
-    text_placement_x = int((new_width - text_img.image.size[0]) / 2)
-    text_placement_y = 0
-
-    if not text_on_top:
-        text_placement_y = im.size[1] + gap
-
-    final_drawing.image.paste(im, (image_placement_x, image_placement_y))
-    final_drawing.image.paste(text_img.image, (text_placement_x, text_placement_y))
-
-    return final_drawing.image
-
-
 font_lg = ImageFont.truetype(ASSET_UBUNTU_MONO, 15)
 font_sm = ImageFont.truetype(ASSET_UBUNTU_MONO, 10)
 
-im1 = add_text_to_image(Image.open("../assets/loading_1.png").convert("1"), "Loading...", font_lg)
-im2 = add_text_to_image(Image.open("../assets/loading_2.png").convert("1"), "Loading...", font_lg)
+im1 = helpers.add_text_to_image(Image.open("../assets/loading_1.png").convert("1"), "Loading...", font_lg)
+im2 = helpers.add_text_to_image(Image.open("../assets/loading_2.png").convert("1"), "Loading...", font_lg)
 
-im1 = add_text_to_image(im1, "PiBike v" + VERSION, font_sm)
-im2 = add_text_to_image(im2, "PiBike v" + VERSION, font_sm)
+im1 = helpers.add_text_to_image(im1, "PiBike v" + VERSION, font_sm)
+im2 = helpers.add_text_to_image(im2, "PiBike v" + VERSION, font_sm)
 
-im1 = grow_to_display_footprint(im1, background=0)
-im2 = grow_to_display_footprint(im2, background=0)
+im1 = helpers.grow_to_display_footprint(im1, background=0)
+im2 = helpers.grow_to_display_footprint(im2, background=0)
 
 
 while True:
-    # Note to self - add delays in the final one
     disp.show_image(im1)
+    time.sleep(0.3)
     disp.show_image(im2)
+    time.sleep(0.3)
 
 
 # This will be useful later on
