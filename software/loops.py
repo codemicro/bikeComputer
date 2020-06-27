4afc083ba4f13cdcb0571e2a0b1b6dc0b2c42288
Tom <tom@tdpain.net>
2020-06-27 14:32:51 +0100

    Switch to use of font constants and MasterClass.ready

--- software/loops.py
+++ software/loops.py
@@ -3,6 +3,7 @@ import time
 from PIL import ImageFont, Image
 from constants import *
 import helpers
+from display import Drawing
 from masterclass import m
 
 
@@ -11,20 +12,30 @@ def loading_animation():
     Displays a loading animation
     """
 
-    font_lg = ImageFont.truetype(ASSET_UBUNTU_MONO, 15)
-    font_sm = ImageFont.truetype(ASSET_UBUNTU_MONO, 10)
+    im1 = helpers.add_text_to_image(Image.open("../assets/loading_1.png").convert("1"), "Loading...",
+                                    ASSET_UBUNTU_MONO_ME)
+    im2 = helpers.add_text_to_image(Image.open("../assets/loading_2.png").convert("1"), "Loading...",
+                                    ASSET_UBUNTU_MONO_ME)
 
-    im1 = helpers.add_text_to_image(Image.open("../assets/loading_1.png").convert("1"), "Loading...", font_lg)
-    im2 = helpers.add_text_to_image(Image.open("../assets/loading_2.png").convert("1"), "Loading...", font_lg)
-
-    im1 = helpers.add_text_to_image(im1, "PiBike v" + VERSION, font_sm)
-    im2 = helpers.add_text_to_image(im2, "PiBike v" + VERSION, font_sm)
+    im1 = helpers.add_text_to_image(im1, "PiBike v" + VERSION, ASSET_UBUNTU_MONO_SM)
+    im2 = helpers.add_text_to_image(im2, "PiBike v" + VERSION, ASSET_UBUNTU_MONO_SM)
 
     im1 = helpers.grow_to_display_footprint(im1, background=0)
     im2 = helpers.grow_to_display_footprint(im2, background=0)
 
-    while True:
+    while not m.ready:
         m.disp.show_image(im1)
         time.sleep(0.3)
         m.disp.show_image(im2)
         time.sleep(0.3)
+
+
+def display_loop():
+    loading_animation()
+
+    d = Drawing()
+    d.draw.text((0, 0), "Ready!", font=ASSET_UBUNTU_MONO_LG, fill=255)
+
+    d.image = helpers.center_image(d.image)
+
+    m.disp.show_drawing(d)
