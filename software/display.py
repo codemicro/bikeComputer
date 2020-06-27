50a924e9eb019e7f4e99deec57225c2087477bba
Tom <tom@tdpain.net>
2020-06-27 14:06:26 +0100

    Remove requirement to pass a Display object to Drawing
    Use blank image constants in Display.clear
    Add display update lock

--- software/display.py
+++ software/display.py
@@ -1,7 +1,10 @@
+import threading
+
 import adafruit_ssd1306
 import board
 from PIL import Image
 from PIL import ImageDraw
+from constants import *
 
 
 """
@@ -9,6 +12,8 @@ Classes and functions related to display drawing/generation
 """
 
 class Display:
+    lock = threading.Lock()
+
     def __init__(self, i2c_addr, disp_width, disp_height):
         self.width = disp_width
         self.height = disp_height
@@ -18,26 +23,25 @@ class Display:
         self.disp.show()
 
     def clear(self, inverted=False):
-        d = Drawing(self)
-        d.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=255 if inverted else 0)
-        self.show_drawing(d)
+        if inverted:
+            self.show_image(UI_INVERTED_BLANK_IMAGE)
+        else:
+            self.show_image(UI_BLANK_IMAGE)
 
     def show_image(self, im):
+        self.lock.acquire()
         self.disp.image(im)
         self.disp.show()
+        self.lock.release()
 
     def show_drawing(self, drawing):
         self.show_image(drawing.image)
 
 
 class Drawing:
-    def __init__(self, display, im=None):
-        self.display = display
+    def __init__(self, im=None):
         if im is None:
-            self.image = Image.new("1", (display.width, display.height))
+            self.image = Image.new("1", (DISPLAY_WIDTH, DISPLAY_HEIGHT))
         else:
             self.image = im
         self.draw = ImageDraw.Draw(self.image)
-
-    def show(self):
-        self.display.show_image(self)
